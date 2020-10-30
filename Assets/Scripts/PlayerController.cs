75de1b3f6e4cb506cf664534ce9f8e284c4b38fa
Andy Lau <pikachupalsmail@gmail.com>
2020-10-30 21:23:04 +0000

    Readded Assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float Speed;
+    private Rigidbody Rb;
+
+    private bool Liquified;
+    private bool LiquifiedOffCooldown;
+
+    private int LiquifiedLength;
+    private int LiquifiedCooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Liquified = false;
+        LiquifiedLength = 1;
+        LiquifiedCooldown = 3;
+        LiquifiedOffCooldown = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (LiquifiedOffCooldown)
+            {
+                StartCoroutine(TimedStateChange());
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void ChangeLiquidState()
+    {
+        Liquified ^= true;
+    }
+
+    private void UpdateLiquifiedCooldown()
+    {
+        LiquifiedOffCooldown ^= true;
+    }
+
+    IEnumerator TimedStateChange()
+    {
+        ChangeLiquidState();
+        UpdateLiquifiedCooldown();
+        yield return new WaitForSeconds(LiquifiedLength);
+        ChangeLiquidState();
+        yield return new WaitForSeconds(LiquifiedCooldown);
+        UpdateLiquifiedCooldown();
+    }
+
+    public bool ReturnState()
+    {
+        return Liquified;
+    }
+
+    public bool ReturnCooldown()
+    {
+        return LiquifiedOffCooldown;
+    }
+}
---
+++ Assets/Scripts/SpawnFloorSprite.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFloorSprite : MonoBehaviour
+{
+
+    public GameObject FloorTile;
+    private EnvironmentController EnvironmentController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnvironmentController = GameObject.FindWithTag("EnvironmentController").GetComponent<EnvironmentController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // When one floor tile leaves, spawn another to replace it. (Continous floor spawning)
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Floor")
+        {
+            GameObject newFloor = Instantiate(FloorTile, new Vector3(14.4f, -1.5f, 0f), Quaternion.identity);
+            newFloor.GetComponent<TileMovement>().SetSpeed(EnvironmentController.GetMovementSpeed());
+        }
+    }
+
+}
