cb74608b875079197a522beccea37eefc1b781bc
AsyncVoid <cookieassassin32@gmail.com>
2020-10-30 22:11:38 +0000

    Update to 2D

---
+++ Assets/Scripts/EnvironmentController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour
+{
+
+    private float InitialMovementSpeed;
+    private float MovementSpeed;
+    public GameObject FloorTile;
+    private List<GameObject> InitialFloorTiles;
+
+    private bool CombatScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CombatScene = false;
+        InitialMovementSpeed = 0.8f;
+        MovementSpeed = InitialMovementSpeed;
+
+        // Create initial floor tiles.
+        foreach (int value in Enumerable.Range(-10, 24)) {
+            Instantiate(FloorTile, new Vector3((float) value + 0.5f, -1.5f, 0f), Quaternion.identity);
+        }
+
+        // Give them movement speed to allow them to scroll.
+        foreach (GameObject floorTile in GameObject.FindGameObjectsWithTag("Floor")) {
+            floorTile.GetComponent<TileMovement>().SetSpeed(MovementSpeed);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Returns movement speed.
+    public float GetMovementSpeed() {
+        return MovementSpeed;
+    }
+
+    // Methods for combat scene initiation (Pause scrolling and background gen).
+    public void TriggerCombatScene() {
+        CombatScene = true;
+        MovementSpeed = 0f;
+
+        foreach (GameObject floorTile in GameObject.FindGameObjectsWithTag("Floor")) {
+            floorTile.GetComponent<TileMovement>().HaltTileMovement();
+        }
+    }
+
+    public void EndCombatScene() {
+        CombatScene = false;
+        MovementSpeed = InitialMovementSpeed;
+
+        foreach (GameObject floorTile in GameObject.FindGameObjectsWithTag("Floor"))
+        {
+            floorTile.GetComponent<TileMovement>().RestoreTileMovement();
+        }
+    }
+
+    public bool GetIsCombatScene() {
+        return CombatScene;
+    }
+
+}
---
+++ Assets/Scripts/TileMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMovement : MonoBehaviour
+{
+
+    private float Speed;
+    private Vector3 TileVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TileVelocity = new Vector3(-1 * Speed, 0, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += TileVelocity * Time.deltaTime;
+    }
+
+    public void SetSpeed(float environmentSpeed) {
+        Speed = environmentSpeed;
+        TileVelocity = new Vector3(-1 * Speed, 0, 0);
+    }
+
+    public void HaltTileMovement() {
+        TileVelocity = new Vector3(0, 0, 0);
+    }
+
+    public void RestoreTileMovement() {
+        TileVelocity = new Vector3(-1 * Speed, 0, 0);
+    }
+}
