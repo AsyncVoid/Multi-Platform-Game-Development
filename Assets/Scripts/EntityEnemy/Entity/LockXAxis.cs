3c80f38e67a632cbca9c54841b2256351f4478be
Andy Lau <pikachupalsmail@gmail.com>
2020-12-06 18:23:54 +0000

    Added fire skill and new consumable

---
+++ Assets/Scripts/EntityEnemy/Entity/LockXAxis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockXAxis : MonoBehaviour
+{
+    private float x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.x = transform.position.x;
+    }
+
+    // Update is called once per frame. Locks x Axis of player.
+    void LateUpdate()
+    { 
+        Vector3 before = transform.position;
+        transform.position = new Vector3(x, before.y);
+    }
+}
