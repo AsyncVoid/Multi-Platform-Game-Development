b797b6f3840f894063a174a9561b73196b0f3b29
saeedashukor <saeedashukor360@gmail.com>
2020-12-06 06:47:17 +0000

    Added UI code
    - Need to fix hotkeys

---
+++ Assets/Scripts/UI/ActiveSkills.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* UI to display active skills chosen by the player */
+public class ActiveSkills : MonoBehaviour
+{
+    private SkillSlot[] skillSlots;
+    private GameObject[] skillSlotObj;
+    private Player player;
+    private MenuController menuController;
+    private SkillList playerSkills;
+    private HotkeyMenu hotkeyMenu;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        playerSkills = player.skills;
+        menuController = GameObject.FindGameObjectWithTag("MenuController").GetComponent<MenuController>();
+
+        skillSlotObj = GameObject.FindGameObjectsWithTag("SkillSlot");
+        skillSlots = new SkillSlot[skillSlotObj.Length];
+
+        for(int i = 0; i<skillSlotObj.Length;i++){
+            skillSlots[i] = skillSlotObj[i].GetComponent<SkillSlot>();
+            //Debug.Log(i);
+        }
+
+    }
+
+
+    /* Set skill icons for active skills */
+    public void SetActiveSkills(string hotkey, Skill skill)
+    {
+        if(menuController.isPaused){
+            for(int i = 0;i<skillSlots.Length;i++){
+                string key = skillSlots[i].hotkey.text;
+                if(key == hotkey){
+                    skillSlots[i].skillIcon.sprite = skill.skillSprite;
+                }
+            }
+
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/UI/SkillItem.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/* Menu items in Skills Menu */
+public class SkillItem : MonoBehaviour
+{
+    public Text skillName;
+    public Image tierOne;
+    public Image tierTwo;
+    public Image tierThree;
+
+    void Start()
+    {
+        skillName = transform.Find("Skill_text").GetComponent<Text>();
+
+        tierOne = transform.Find("1").GetComponent<Image>();
+        tierTwo = transform.Find("2").GetComponent<Image>();
+        tierThree = transform.Find("3").GetComponent<Image>();
+
+        tierOne.gameObject.SetActive(true);
+        tierTwo.gameObject.SetActive(true);
+        tierThree.gameObject.SetActive(true);
+
+        tierOne.color = Color.clear;
+        tierTwo.color = Color.clear;
+        tierThree.color = Color.clear;   
+    }
+
+    public void UpdateSkillTier(string skillname, int skilltier)
+    {
+        Color32 c1 = new Color32(0,191,255,255);
+        Color32 c2 = new Color32(0,115,255,255);
+
+        skillName.text = skillname;
+
+        if(skilltier == 1){
+            Debug.Log(skillname+": Tier 1");
+            tierOne.color = Color.white;
+            tierTwo.color = Color.clear;
+            tierThree.color = Color.clear;
+        }
+        else if(skilltier == 2){
+            Debug.Log(skillname+": Tier 2");
+            tierOne.color = Color.white;
+            tierTwo.color = c1;
+            tierThree.color = Color.clear;
+        }
+        else if(skilltier == 3){
+            Debug.Log(skillname+": Tier 3");
+            tierOne.color = Color.white;
+            tierTwo.color = c1;
+            tierThree.color = c2;
+        }
+        else{
+            tierOne.color = Color.clear;
+            tierTwo.color = Color.clear;
+            tierThree.color = Color.clear;     
+        }
+
+        
+    }
+}
