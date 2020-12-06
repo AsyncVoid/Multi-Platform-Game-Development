7e4259a8bcf3ef2d24c0fbe3ef4154c96d47acdb
Andy Lau <pikachupalsmail@gmail.com>
2020-12-06 22:09:29 +0000

    Added on fire sound for ice shard.

---
+++ Assets/Scripts/Skills/SkillBehaviour/IceShard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceShard : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip iceCharge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ChargeSound() {
+        audioSource.PlayOneShot(iceCharge, 1f);
+    }
+}
